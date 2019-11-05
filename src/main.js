568bd30c6f3e2a1ae65d9c63464f20d35a04fb38
kbudk <kbudk@BLK2512>
2019-11-05 16:14:33 +0100

    s

--- src/main.js
+++ src/main.js
@@ -3,11 +3,17 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import VueMapbox from "../node_modules/vue-mapbox";
+import Mapbox from "mapbox-gl";
+import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueMapbox, { mapboxgl: Mapbox });
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
