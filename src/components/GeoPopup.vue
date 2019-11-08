84a42a9c7a6c86ffce978087f9842896471c2ecf
kbudk <kbudk@BLK2512>
2019-11-08 09:27:03 +0100

    Prettier run

--- src/components/GeoPopup.vue
+++ src/components/GeoPopup.vue
@@ -1,61 +1,62 @@
-
 <template lang="pug">
-mgl-popup(
-  ref="popup",
-  :coordinates="getCoordinates",
-  :closeButton="closeButton",
-  :closeOnClick="closeOnClick")
-  .map-popup-content.has-text-centered
+  mgl-popup(
+    ref="popup",
+    :coordinates="getCoordinates",
+    :closeButton="closeButton",
+    :closeOnClick="closeOnClick")
+    .map-popup-content.has-text-centered
     strong {{ content}}
 </template>
 
 <script>
-import { MglPopup } from 'vue-mapbox';
+import { MglPopup } from "vue-mapbox";
 
 export default {
-  name: 'GeoPopup',
+  name: "GeoPopup",
   components: {
-    MglPopup,
+    MglPopup
   },
   props: {
     coordinates: {
       type: Array,
-     default: undefined, /* this will not initialise the popup when you add it */
+      default: undefined /* this will not initialise the popup when you add it */
     },
     content: {
       type: Object,
       default() {
         return {};
-      },
+      }
     },
     show: {
       type: Boolean,
-      default: true,
-    },
+      default: true
+    }
   },
   data: () => ({
     popupContent: null,
     closeButton: false,
-    closeOnClick: false,
+    closeOnClick: false
   }),
   watch: {
     show() {
       if (!this.show) {
-        if (this.$refs.popup) { this.$refs.popup.popup.remove(); }
-      } else if (this.$refs.popup) { this.$refs.popup.popup.addTo(this.$store.map); }
-    },
+        if (this.$refs.popup) {
+          this.$refs.popup.popup.remove();
+        }
+      } else if (this.$refs.popup) {
+        this.$refs.popup.popup.addTo(this.$store.map);
+      }
+    }
   },
   computed: {
     getCoordinates() {
       // Here we can do stuff with the coordinates if needed
       // ...
       return this.coordinates;
-    },
+    }
   },
-  methods: {
-  },
+  methods: {}
 };
 </script>
-
 <style scoped>
 </style>
